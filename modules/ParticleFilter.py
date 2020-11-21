674af9ef4343b03fb7cb5d83ad67b1ad57783a54
Adam Thahir <adam.thahir@gmail.com>
2020-11-21 23:34:28 +0300

    Particle Fitler (attempt)

--- modules/ParticleFilter.py
+++ modules/ParticleFilter.py
@@ -1,25 +1,72 @@
 import numpy as np
-import tf
-import rospy
+from numpy.random import uniform
 
-from std_msgs.msg import Header, String, ColorRGBA
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, PoseArray, Pose, Point, Quaternion, Vector3
-from nav_msgs.srv import GetMap
-from visualization_msgs.msg import Marker, MarkerArray
-from numpy.random import random_sample, normal
-from tf import TransformListener
-from tf import TransformBroadcaster
-from tf.transformations import euler_from_quaternion, rotation_matrix, quaternion_from_matrix
-from random import gauss
+class Particle:
+    # Main concepts obtained from https://github.com/Kalman-and-Bayesian-Filters-in-Python/
+    def __init__(self):
+        pass
 
-class Particle(object):
-    def __init__(self, x=0.0, y=0.0, theta=0.0, w=1.0):
-        self.x = x
-        self.y = y
-        self.theta = theta
-        self.w = w
+    def create_uniform_particles(self, x_range, y_range, hdg_range, N):
+        particles = np.empty((N, 3))
+        particles[:, 0] = uniform(x_range[0], x_range[1], size=N)
+        particles[:, 1] = uniform(y_range[0], y_range[1], size=N)
+        particles[:, 2] = uniform(hdg_range[0], hdg_range[1], size=N)
+        particles[:, 2] %= 2 * np.pi
+        return particles
 
-    def as_pose(self):
-        orientation_tuple = tf.transformations.quaternion_from_euler(0,0,self.theta)
-        return Pose(position=Point(x=self.x,y=self.y,z=0), orientation=Quaternion(x=orientation_tuple[0], y=orientation_tuple[1], z=orientation_tuple[2], w=orientation_tuple[3]))
+    def create_gaussian_particles(mean, std, N):
+        particles = np.empty((N, 3))
+        particles[:, 0] = mean[0] + (randn(N) * std[0])
+        particles[:, 1] = mean[1] + (randn(N) * std[1])
+        particles[:, 2] = mean[2] + (randn(N) * std[2])
+        particles[:, 2] %= 2 * np.pi
+        return particles
+
+    def predict(self, particles, u, std, dt=1.):
+        N = len(particles)
+        
+        particles[:, 2] += u[0] + (randn(N) * std[0])
+        particles[:, 2] %= 2 * np.pi
+
+
+        dist = (u[1] * dt) + (randn(N) * std[1])
+        particles[:, 0] += np.cos(particles[:, 2]) * dist
+        particles[:, 1] += np.sin(particles[:, 2]) * dist
+
+        return particles
+
+    def update(self, particles, weights, z, R, landmarks):
+        for i, landmark in enumerate(landmarks):
+            distance = np.linalg.norm(particles[:, 0:2] - landmark, axis=1)
+            weights *= scipy.stats.norm(distance, R).pdf(z[i])
+
+        weights += 1.e-300     
+        weights /= sum(weights)
+
+    def estimate(self, particles, weights):
+        pos = particles[:, 0:2]
+        mean = np.average(pos, weights=weights, axis=0)
+        var  = np.average((pos - mean)**2, weights=weights, axis=0)
+
+        return mean, var
+
+    def simple_resample(self, particles, weights):
+        N = len(particles)
+        cumulative_sum = np.cumsum(weights)
+        cumulative_sum[-1] = 1. # avoid round-off error
+        indexes = np.searchsorted(cumulative_sum, random(N))
+
+        # resample according to indexes
+        particles[:] = particles[indexes]
+        weights.fill(1.0 / N)
+
+    def neff(self, weights):
+        return 1. / np.sum(np.square(weights))
+
+    def resample_from_index(self, particles, weights, indexes):
+        particles[:] = particles[indexes]
+        weights.resize(len(particles))
+        weights.fill (1.0 / len(weights))
+
+        return particles, weights
+    
